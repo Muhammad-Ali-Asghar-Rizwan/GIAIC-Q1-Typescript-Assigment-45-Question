d0045fc5e3ef74312ac5fd93520d38c02b2c9f90
Muhammad-Ali-Asghar-Rizwan <164878048+Muhammad-Ali-Asghar-Rizwan@users.noreply.github.com>
2024-11-20 01:46:02 +0500

    first commit

---
+++ Q2.js
@@ -0,0 +1,10 @@
+#! /usr/bin/env node
+"use strict";
+/*2. Personal Message:
+Store a person’s name in a variable, and print a message to that person. Your message should
+be simple, such as, “Hello Eric, would you like to learn some Python today?”*/
+Object.defineProperty(exports, "__esModule", { value: true });
+const personName = " would you like to learn some Python today?”";
+console.log(`“Hello Eric,${personName}`);
+const Q3_1 = require("./Q3");
+console.log(Q3_1.lowercase);
